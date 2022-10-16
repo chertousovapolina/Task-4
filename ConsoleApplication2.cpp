475749fbb410e78794011ffbceebecbd5baf83dc
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-16 16:12:32 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,25 @@
+﻿// Chertousova PE
+// 16.10.2022
+// Task 4
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int  N;
+	cin >> N;
+	if (N == 12 || N == 1 || N == 2)
+		cout << "Winter";
+
+	if (N == 3 || N == 4 || N == 5)
+		cout << "Spring";
+
+	if (N == 6 || N == 7 || N == 8)
+		cout << "Summer";
+
+	if (N == 9 || N == 10 || N == 11)
+		cout << "Autumn";	
+	if (N > 12)
+		cout << "Error";
+}
